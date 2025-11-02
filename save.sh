b572249aeef5535a40207f80d48f4cfb1681ce26
min-lfl <2775381313@qq.com>
2025-11-02 13:37:51 +0800

    更新了一下推送脚本，增加了推送所有分支的功能，并且把关于分支的命令写入了md里

--- save.sh
+++ save.sh
@@ -4,12 +4,13 @@ git commit -m "$1"
 
 # 检查提交是否成功
 if [ $? -eq 0 ]; then
-    echo "提交成功，开始推送..."
-    git push github master
+    echo "提交成功，开始推送所有分支..."
+    # 推送所有分支到 github 远程仓库
+    git push github --all
     if [ $? -eq 0 ]; then
-        echo "推送完成: $1"
+        echo "推送所有分支完成: $1"
     else
-        echo "推送失败，请检查网络或冲突: $1"
+        echo "推送所有分支失败，请检查网络或冲突: $1"
     fi
 else
     echo "提交失败: $1"
