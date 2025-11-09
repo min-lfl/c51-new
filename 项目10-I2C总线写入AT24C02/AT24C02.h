f0440b511bba993422f20e97753990f4cc45c36e
min-lfl <2775381313@qq.com>
2025-11-09 16:06:51 +0800

    完成了秒表案例，按钮1位启停，按钮2为清零，按钮3为存储计数，按钮4为读取计数（断电不丢失）

--- 项目10-I2C总线写入AT24C02/AT24C02.h
+++ 项目10-I2C总线写入AT24C02/AT24C02.h
@@ -1,7 +1,10 @@
 #ifndef __AT24C02_H__
 #define __AT24C02_H__
 
+//在哪个位置写什么，返回值表示是否写入超时，1代表超时，0代表正常写入
 bit write_AT24C02(unsigned char ADDRESS,Byte);
+
+//在什么地方读取，返回值是读取到的字节
 unsigned char  read_AT24C02(unsigned char ADDRESS);
 
 
--- 项目10-I2C总线写入AT24C02/main.c
+++ 项目10-I2C总线写入AT24C02/main.c
@@ -9,10 +9,9 @@
 #include <888led.H>
 
 
-unsigned char key,i;
-unsigned char h=7,m=23,date=55;
-unsigned int T0Count2;
-
+unsigned char key;
+unsigned char Min,Sec,Minisec;//分，秒，毫秒
+unsigned char start;
 
 void main(){
 	Timer0Init();
@@ -21,54 +20,63 @@ void main(){
 		key=switch0000();
 		if(key!=0){
 			if(key==1){
-				date+=1;
-				T0Count2=0;
+				start=~start;//取反控制启停
 			}
-			if(key==2){
-				date-=1;
-				T0Count2=0;
+			if(key==2){// 清零
+				Min=0;
+				Sec=0;
+				Minisec=0;
 			}
 			if(key==3){
-				date=3;
+				write_AT24C02(1,Min);
+				write_AT24C02(2,Sec);
+				write_AT24C02(3,Minisec);
 			}
 			if(key==4){
-				date=4;
+				Min=read_AT24C02(1);
+				Sec=read_AT24C02(2);
+				Minisec=read_AT24C02(3);
 			}
 		}
 		
-		
-		if(date>=60){
-			date=0;
-			m++;
-			if(m>=60){
-				m=0;
-				h++;
-				if(h>=24){
-					h=0;
-				}
-			}
-		}
-		Set_888led(1,h/10);
-		Set_888led(2,h%10);
+		Set_888led(1,Min/10);
+		Set_888led(2,Min%10);
 		
 		Set_888led(3,11);
 		
-		Set_888led(4,m/10);
-		Set_888led(5,m%10);
+		Set_888led(4,Sec/10);
+		Set_888led(5,Sec%10);
 		
 		Set_888led(6,11);
 		
-		Set_888led(7,date/10);
-		Set_888led(8,date%10);
+		Set_888led(7,Minisec/10);
+		Set_888led(8,Minisec%10);
 		
 	}
 }
 
-
+//自加函数，会自动进位，执行一次自加一次
+void sec_add(){
+	//停止计时控制
+	if(start){
+		Minisec++;
+		if(Minisec>=100){
+			Minisec=0;
+			Sec++;
+			if(Sec>=60){
+				Sec=0;
+				Min++;
+				if(Min>=60){
+					Min=0;
+				}
+			}
+		}
+	}
+}
 
 void Timer0_Routine() interrupt 1
 {
-	static  unsigned int T0Count,T0Count1;
+	static  unsigned int T0Count,T0Count1,T0Count2;
 
 	TL0 = 0x18;		//设置定时初值
 	TH0 = 0xFC;		//设置定时初值
@@ -85,9 +93,9 @@ void Timer0_Routine() interrupt 1
 		T0Count1=0;
 		Scan_888led();
 	}
-	if(T0Count2>=1000)  //1000毫秒秒表加1
+	if(T0Count2>=10)  //10毫秒秒表加1
 	{
 		T0Count2=0;
-		date++;
+		sec_add();
 	}
 }
