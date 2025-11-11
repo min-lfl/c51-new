28dbb4a3ea7bfb3360c2b8798556d52688529d05
min-lfl <2775381313@qq.com>
2025-11-11 19:52:24 +0800

    温度传感器项目完成，模块封装无误

--- 项目11-DS18B20温度传感器显示/main.c
+++ 项目11-DS18B20温度传感器显示/main.c
@@ -7,21 +7,9 @@
 #include <LCD1602.H>
 #include <DS18B20.H>
 
-unsigned int Time;//原始数据
-unsigned char Temp_Int,Temp_Float; //温度的整数部分和小数部分
 
-unsigned char reverse_low_4bits(unsigned char byte) {
-    unsigned char high = byte & 0xF0;    // 保存高4位
-    unsigned char low = byte & 0x0F;     // 取低4位
-    
-    // 反转低4位：00001011 -> 低4位1011 -> 1101
-    low = ((low & 0x1) << 3) | 
-          ((low & 0x2) << 1) | 
-          ((low & 0x4) >> 1) | 
-          ((low & 0x8) >> 3);
-    
-    return high | low;  // 0000 | 1101 = 00001101
-}
+unsigned char Temp_Int; //温度的整数部分
+unsigned int 	Temp_Float;//温度的小数部分
 
 
 void main(){
@@ -29,14 +17,16 @@ void main(){
 	
 	
 	while(1){
-		Start_DS18C02();
-		Time=Read_DS18C02();
-		Temp_Int = Time >> 4;
-		Temp_Float= reverse_low_4bits((Time << 12) >> 12);
+
+		DS18C02_Handledate_read(&Temp_Int,&Temp_Float);
+		
 		LCD_ShowString(1,1,"Celsius:");
 		LCD_ShowNum(2,1,Temp_Int,2);
 		LCD_ShowString(2,3,".");
-		LCD_ShowNum(2,4,Temp_Float,2);
-		LCD_ShowString(2,6,"C");
+		LCD_ShowNum(2,4,Temp_Float,4);
+		LCD_ShowString(2,8,"C");
+		
+		
+
 	}
 }
